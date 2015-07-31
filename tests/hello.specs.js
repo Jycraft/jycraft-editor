1626fe17fba627a008f3e430f81e5e7f2741696c
Paul Everitt <paul@agendaless.com>
2015-07-31 12:02:12 +0200

    First start at Mocha tests.

---
+++ tests/hello.specs.js
@@ -0,0 +1,8 @@
+import chai from "chai";
+let expect = chai.expect;
+
+describe('The ping method', function () {
+    it('can compile a template string', function () {
+        expect(1).to.equal(1);
+    });
+});
