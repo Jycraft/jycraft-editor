48cf2cd3503c9ca5a552c80eb75ecb4699feb259
Paul Everitt <paul@agendaless.com>
2015-08-01 18:04:17 +0200

    Test as a constructor.

--- src/features/home/tests/home.controller.specs.js
+++ src/features/home/tests/home.controller.specs.js
@@ -4,6 +4,10 @@ import Controller from "../home.controller";
 
 describe("Controller", () => {
     it("can be imported", () => {
-        expect(Controller).to.be.an('function');
+        expect(Controller).to.be.an("function");
+    });
+    it("can be instantiated", () => {
+        let obj = new Controller();
+        expect(obj).to.be.an("object");
     });
 });
