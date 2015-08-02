1d246e38a98a61a0d5fc72066a7e1789804fdb33
Paul Everitt <paul@agendaless.com>
2015-08-02 11:20:05 +0200

    A bit more test coverage.

--- src/features/sessions/tests/sessions.controller.specs.js
+++ src/features/sessions/tests/sessions.controller.specs.js
@@ -2,8 +2,26 @@ import { expect } from "chai";
 
 import Controller from "../sessions.controller";
 
-describe("Controller", () => {
-    it("can be imported", () => {
-        expect(Controller).to.be.an('function');
+describe.only("Controller", () => {
+
+    let $log, $rootScope, connection, $mdToast, editor, ctrl;
+
+    beforeEach(() => {
+        ctrl = new Controller($log, $rootScope, connection, $mdToast);
     });
+
+    it("can be imported", () => {
+        expect(Controller).to.be.an("function");
+    });
+    
+    it("can be constructed", () => {
+        expect(ctrl).to.be.an("object");
+    });
+
+    it("ACE to be configured", () => {
+        expect(ctrl.aceConfig).to.be.an("object");
+        expect(ctrl.aceConfig.onLoad).to.be.an("function");
+        expect(ctrl.aceConfig.onChange).to.be.an("function");
+    });
+
 });
