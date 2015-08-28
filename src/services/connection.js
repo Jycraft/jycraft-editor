844e49dfdac6a5fa3faa47d6e73bb80d92feeb4e
Paul Everitt <paul@agendaless.com>
2015-08-28 10:55:20 -0400

    Clean up the error codes a bit.

--- src/services/connection.js
+++ src/services/connection.js
@@ -32,21 +32,6 @@ class Connection {
                 ` sending 'login!<PASSWORD>'`) {
                 ctrl.isConnected = false;
                 ctrl.loginFailed = true;
-            } else if (response == "$") {
-                // Here's the fun stuff, handle a series of characters in
-                // the response, starting and finishing with $, and
-                // treat those in between as a JSON stream. Gotta fix
-                // the server plugin.
-                if (fullResponse.isStarted) {
-                    // We received our second $, let's emit a message
-                    $rootScope.$broadcast("JsonResponse", JSON.parse(fullResponse.value));
-                    fullResponse.isStarted = false;
-                    fullResponse.value = "";
-                } else {
-                    fullResponse.isStarted = true;
-                }
-            } else if (fullResponse.isStarted && response != "$") {
-                fullResponse.value += response;
             } else {
                 ctrl.isConnected = true;
                 ctrl.loginFailed = false;
@@ -61,35 +46,9 @@ class Connection {
 
     onClose(event) {
         this.isConnected = false;
-        let reason;
-        if (event.code == 1000)
-            reason = "Normal closure, meaning that the purpose for which the connection was established has been fulfilled.";
-        else if (event.code == 1001)
-            reason = "An endpoint is \"going away\", such as a server going down or a browser having navigated away from a page.";
-        else if (event.code == 1002)
-            reason = "An endpoint is terminating the connection due to a protocol error";
-        else if (event.code == 1003)
-            reason = "An endpoint is terminating the connection because it has received a type of data it cannot accept (e.g., an endpoint that understands only text data MAY send this if it receives a binary message).";
-        else if (event.code == 1004)
-            reason = "Reserved. The specific meaning might be defined in the future.";
-        else if (event.code == 1005)
-            reason = "No status code was actually present.";
-        else if (event.code == 1006)
-            reason = "The connection was closed abnormally, e.g., without sending or receiving a Close control frame";
-        else if (event.code == 1007)
-            reason = "An endpoint is terminating the connection because it has received data within a message that was not consistent with the type of the message (e.g., non-UTF-8 [http://tools.ietf.org/html/rfc3629] data within a text message).";
-        else if (event.code == 1008)
-            reason = "An endpoint is terminating the connection because it has received a message that \"violates its policy\". This reason is given either if there is no other sutible reason, or if there is a need to hide specific details about the policy.";
-        else if (event.code == 1009)
-            reason = "An endpoint is terminating the connection because it has received a message that is too big for it to process.";
-        else if (event.code == 1010) // Note that this status code is not used by the server, because it can fail the WebSocket handshake instead.
-            reason = "An endpoint (client) is terminating the connection because it has expected the server to negotiate one or more extension, but the server didn't return them in the response message of the WebSocket handshake. <br /> Specifically, the extensions that are needed are: " + event.reason;
-        else if (event.code == 1011)
-            reason = "A server is terminating the connection because it encountered an unexpected condition that prevented it from fulfilling the request.";
-        else if (event.code == 1015)
-            reason = "The connection was closed due to a failure to perform a TLS handshake (e.g., the server certificate can't be verified).";
-        else
-            reason = "Unknown reason";
+        let code = event.code;
+        let reason = closeErrors[code] ? closeErrors[code] : "Unknown reason";
+
         this.toast("Connection closed: " + reason);
 
     }
@@ -107,9 +66,44 @@ class Connection {
     toast(message) {
         this.$mdToast.show(this.$mdToast.simple().content(message));
     }
-
 }
 
 Connection.$inject = ["$log", "$rootScope", "$websocket", "$mdToast"];
 
 export default Connection;
+
+// Error code mapping data
+var closeErrors = {
+    1000: `Normal closure, meaning that the purpose for which the
+    connection was established has been fulfilled.`,
+    1001: `An endpoint is "going away", such as a server going down or a
+    browser having navigated away from a page.`,
+    1002: `An endpoint is terminating the connection due to a protocol error`,
+    1003: `An endpoint is terminating the connection because it has
+     received a type of data it cannot accept (e.g., an endpoint that
+     understands only text data MAY send this if it receives a binary
+     message).`,
+    1004: `Reserved. The specific meaning might be defined in the future.`,
+    1005: `No status code was actually present.`,
+    1006: `The connection was closed abnormally, e.g., without sending
+    or receiving a Close control frame`,
+    1007: `An endpoint is terminating the connection because it has
+    received data within a message that was not consistent with the type
+    of the message (e.g., non-UTF-8 [http://tools.ietf.org/html/rfc3629]
+    data within a text message).`,
+    1008: `An endpoint is terminating the connection because it has
+    received a message that "violates its policy". This reason is
+    given either if there is no other suitable reason, or if there is a
+    need to hide specific details about the policy.`,
+    1009: `An endpoint is terminating the connection because it has
+    received a message that is too big for it to process.`,
+    1010: `An endpoint (client) is terminating the connection because
+    it has expected the server to negotiate one or more extension, but
+    the server didn't return them in the response message of the
+    WebSocket handshake.`,
+    1011: `A server is terminating the connection because it encountered
+    an unexpected condition that prevented it from fulfilling the
+    request.`,
+    1015: `The connection was closed due to a failure to perform a TLS handshake (e.g., the server certificate can't be verified).`
+
+}
