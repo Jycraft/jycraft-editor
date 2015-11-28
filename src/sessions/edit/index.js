c52760805c1324e133f3bc407cba28f9a70b6d60
Paul Everitt <paul@agendaless.com>
2015-11-28 12:34:53 -0500

    No longer using sessions.

--- src/sessions/edit/index.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-import uirouter from 'angular-ui-router';
-
-import {states} from './edit.states';
-
-export default angular.module('app.sessions.edit', [uirouter])
-    .config(states)
-    .name;
--- src/sessions/tests/sessions.states.specs.js
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-
-import {states} from '../sessions.states';
-
-describe('states', () => {
-    it('can be imported', () => {
-        expect(states).to.be.an('function');
-    });
-});
